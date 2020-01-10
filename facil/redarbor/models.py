6fc13d4de8b95af87663df0daa7b693ff080a6f3
Raul <rblanxart@externos.itnow.es>
2020-01-10 14:47:39 +0100

    v4
    retornat asignacions variables al models

--- facil/redarbor/models.py
+++ facil/redarbor/models.py
@@ -7,15 +7,15 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # user info
     Email = db.Column(db.String(50), nullable=False) 
-    Fax = db.Column(db.String(9), nullable=False)  
+    Fax = db.Column(db.String(9), nullable=True)  
     Name = db.Column(db.String(50), nullable=True)
-    Telephone = db.Column(db.String(9), nullable=False)
+    Telephone = db.Column(db.String(9), nullable=True)
 
     # metadata
-    CreatedOn = db.Column(db.DateTime(), nullable=False, default=db.func.current_timestamp())
-    UpdatedOn = db.Column(db.DateTime(), nullable=False, default=db.func.current_timestamp())
+    CreatedOn = db.Column(db.DateTime(), nullable=True, default=db.func.current_timestamp())
+    UpdatedOn = db.Column(db.DateTime(), nullable=True, default=db.func.current_timestamp())
     DeletedOn = db.Column(db.DateTime(), nullable=True, default=db.func.current_timestamp())
-    Lastlogin = db.Column(db.DateTime(), nullable=False, default=db.func.current_timestamp())
+    Lastlogin = db.Column(db.DateTime(), nullable=True, default=db.func.current_timestamp())
 
     # login
     Username = db.Column(db.String(50), nullable=False)
@@ -26,21 +26,21 @@ class Employee(db.Model):
     PortalId = db.Column(db.Integer, nullable=False)
     RoleId = db.Column(db.Integer, nullable=False)
     StatusId = db.Column(db.Integer, nullable=False)
-    
-    # def __init__(self, dict):
-    #     self.Email = dict.get('email', '')
-    #     self.Fax = dict.get('fax', '')
-    #     self.Name = dict.get('name', '')
-    #     self.Telephone = dict.get('telephone', '')
-    #     self.Username = dict.get('username', '')
-    #     self.Password = dict.get('password', '')
-    #     self.CompanyId = dict.get('companyId')
-    #     self.PortalId = dict.get('portalId', '')
-    #     self.RoleId = dict.get('roleId', '')
-    #     self.StatusId = dict.get('statusId', '')
+ 
+
+    def __init__(self, dict):
+        self.Email = dict.get('email', '')
+        self.Fax = dict.get('fax', '')
+        self.Name = dict.get('name', '')
+        self.Telephone = dict.get('telephone', '')
+        self.Username = dict.get('username', '')
+        self.Password = dict.get('password', '')
+        self.CompanyId = dict.get('companyId')
+        self.PortalId = dict.get('portalId', '')
+        self.RoleId = dict.get('roleId', '')
+        self.StatusId = dict.get('statusId', '')
         
     def save(self):
-        print('id:', id)
         if not self.id:
             db.session.add(self)
         db.session.commit()
